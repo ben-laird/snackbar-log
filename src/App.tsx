16220cd5d54bc5a59c8638e645bee42dcc65cba6
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-07-11 22:50:39 -0500

    Integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import { FC } from "react";
+import { createTheme, CssBaseline, Paper, ThemeProvider } from "@mui/material";
+import { FC, useState } from "react";
+import Logger from "./components/Logger";
+import Mode from "./components/Mode";
+import Title from "./components/Title";
+
+const lightTheme = createTheme({ palette: { mode: "light" } });
+const darkTheme = createTheme({ palette: { mode: "dark" } });
+
+const chooseTheme = (lightMode: boolean) =>
+  lightMode ? lightTheme : darkTheme;
 
 const App: FC = () => {
-  return <div>App</div>;
+  const lightModeState = useState(false);
+
+  return (
+    <ThemeProvider theme={chooseTheme(lightModeState[0])}>
+      <CssBaseline />
+      <Paper elevation={1} sx={{ margin: 3 }}>
+        <Title
+          title="Snackbar.log"
+          description="Simple console viewer for the browser"
+        />
+        <Mode lightModeState={lightModeState} />
+        <Logger callback={() => console.log("Button clicked!")} />
+      </Paper>
+    </ThemeProvider>
+  );
 };
 
 export default App;
