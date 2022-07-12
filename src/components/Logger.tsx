7efd6bef0f19063f7ce3dc43ba4696cf9e1dbc9e
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-07-11 22:50:02 -0500

    Add Logger form

---
+++ src/components/Logger.tsx
@@ -0,0 +1,22 @@
+import { Box, Button, TextField } from "@mui/material";
+import { FC } from "react";
+
+interface LoggerProps {
+  callback: () => void;
+}
+
+const Logger: FC<LoggerProps> = ({ callback }) => (
+  <Box component="form" padding={2} noValidate autoComplete="off" onSubmit={callback}>
+    <TextField
+      label="Text to Log"
+      helperText="Log some text to the Snackbar Console"
+      variant="outlined"
+      fullWidth
+      multiline
+      margin="dense"
+    />
+    <Button onClick={callback}>Submit</Button>
+  </Box>
+);
+
+export default Logger;
